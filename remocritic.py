b128c7293367ec958f35df5ed45756ad3a446b82
Amr-Abdelbar <amrfabdelbar@gmail.com>
2024-10-15 09:26:02 +0300

    more changes, added jinja template

--- remocritic.py
+++ remocritic.py
@@ -1,27 +1,39 @@
 import datetime
+import json
 import matplotlib as plt
 import numpy as np
-import flask
-app = flask.Flask("RemoCritic")
+from flask import Flask, render_template
+import requests
+
+app = Flask("RemoCritic")
 
 def get_html(file_name):
     html_file = open(file_name + ".html")
     content = html_file.read()
     html_file.close()
+    return content
 
 @app.route("/")
 def homepage():
-    return get_html("index")
 
-@app.route("/results")
-def results():
-    return get_html("results")
+    url = "https://opencritic-api.p.rapidapi.com/game/reviewed-this-week"
+    headers = {
+	"x-rapidapi-key": "a374990eafmsh0e1261ce05bf6f1p1c7012jsn9edd139d87b7",
+	"x-rapidapi-host": "opencritic-api.p.rapidapi.com"
+    }
+    response = requests.get(url, headers=headers)
 
-@app.route("/browse")
-def results():
-    return get_html("browse")
+    return render_template("index.html", content = response.json())
 
-@app.route("/localLib")
-def results():
-    return get_html("localLib")
+@app.route("/searchResults")
+def search_results():
+    return get_html("searchResults")
+
+@app.route("/browseAll")
+def browse_all():
+    return get_html("browseAll")
+
+@app.route("/localUserLibrary")
+def local_user_lib():
+    return get_html("localUserLibrary")
 
