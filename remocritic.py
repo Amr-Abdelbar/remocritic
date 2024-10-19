d8d1d4a288f80d5596f7254dfe3e052c7397c4c2
Amr-Abdelbar <amrfabdelbar@gmail.com>
2024-10-19 18:56:20 +0300

    Fixing search function

--- remocritic.py
+++ remocritic.py
@@ -2,10 +2,11 @@ import datetime
 import json
 import matplotlib as plt
 import numpy as np
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, session
 import requests
 
 app = Flask("RemoCritic")
+app.secret_key = "ua6.czP7*~KSX_4#ym[U"
 
 def get_html(file_name):
     html_file = open(file_name + ".html")
@@ -15,12 +16,14 @@ def get_html(file_name):
 
 @app.route("/", methods = ["GET", "POST"] )
 def homepage():
-    
-    if request.method == 'GET':
-        headers = {
+
+    headers = {
         "x-rapidapi-key": "a374990eafmsh0e1261ce05bf6f1p1c7012jsn9edd139d87b7",
         "x-rapidapi-host": "opencritic-api.p.rapidapi.com"
         }
+    
+    if request.method == 'GET':
+
 
         week_url = "https://opencritic-api.p.rapidapi.com/game/reviewed-this-week"
         upcoming_url = "https://opencritic-api.p.rapidapi.com/game/upcoming"
@@ -44,23 +47,23 @@ def homepage():
         querystring = {"criteria":search_value}
         url = "https://opencritic-api.p.rapidapi.com/game/search"
         search_response = requests.get(url, headers=headers, params=querystring).json()
-        print(search_response)
+        session['search_results'] = search_response
 
-        return redirect(url_for('searchResults', search_response=search_response))
+        return redirect(url_for('search_results'))
 
-@app.route("/searchResults/<result>")
-def search_results(result):
+@app.route("/searchResults")
+def search_results():
+    search_response = session.get("search_results", [])
 
-
-    return render_template("searchResults", result=result)
+    return render_template("searchResults.html", result = search_response)
 
 @app.route("/browseAll")
 def browse_all():
-    return render_template("browseAll")
+    return render_template("browseAll.html")
 
 @app.route("/localUserLibrary")
 def local_user_lib():
-    return render_template("localUserLibrary")
+    return render_template("localUserLibrary.html")
 
 @app.route("/game")
 def game():
