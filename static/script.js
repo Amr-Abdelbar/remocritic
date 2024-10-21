b60c5e9e301354480b4ab1dcf0f02e4191da652e
Amr-Abdelbar <amrfabdelbar@gmail.com>
2024-10-21 14:54:26 +0300

    more localstorage

--- static/script.js
+++ static/script.js
@@ -3,3 +3,10 @@ document.addEventListener("DOMContentLoaded", ()=> {
     const userLibrary = document.getElementById("localUserLibrary");
 })
 
+if (localStorage.getItem("userLibrary") == null) {
+    localStorage.setItem("userLibrary" = toString(userLibrary))
+}
+
+else {
+    userLibrary = localStorage.getItem("userLibrary");
+}
