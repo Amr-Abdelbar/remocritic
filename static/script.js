463e672cfd970225dd3496f2e852ddbf5cd7095c
Amr-Abdelbar <amrfabdelbar@gmail.com>
2024-10-21 12:12:03 +0300

    more JS

--- static/script.js
+++ static/script.js
@@ -1,3 +1,3 @@
-document.addEventListener(onload, ()=> {
-    const gameCard = document.getElementsByClassName("gameCard");
+document.addEventListener("DOMContentLoaded", ()=> {
+    const gameCard = document.getElementsByClassName("gameList");
 })
