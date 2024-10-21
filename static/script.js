8fc3154c9af2073ec0481bf8a7868fc887c2c300
Amr-Abdelbar <amrfabdelbar@gmail.com>
2024-10-21 15:39:17 +0300

    more login stuff

--- static/script.js
+++ static/script.js
@@ -2,12 +2,21 @@ document.addEventListener("DOMContentLoaded", ()=> {
     const gameCard = document.getElementsByClassName("gameList");
     const userLibrary = document.getElementById("localUserLibrary");
     const addLibButton = document.getElementsByClassName("addToLibrary");
+    const loginButton = document.getElementById("loginButton");
+    const login = document.getElementById("login");
 })
 
 if (localStorage.getItem("userLibrary") == null) {
-    localStorage.setItem("userLibrary", toString(userLibrary))
+    localStorage.setItem("userLibrary", toString(userLibrary));
 }
 
 else {
     userLibrary = localStorage.getItem("userLibrary");
 }
+
+loginButton.addEventListener(click, ()=> {
+    const userName = login.value;
+    if (userName){
+        localStorage.setItem("userName", userName);
+    }
+})
