a988c7ac8f51da98c510b750e35ebf1ed2c6fbfd
Amr-Abdelbar <amrfabdelbar@gmail.com>
2024-10-21 15:29:16 +0300

    login

--- static/script.js
+++ static/script.js
@@ -1,6 +1,7 @@
 document.addEventListener("DOMContentLoaded", ()=> {
     const gameCard = document.getElementsByClassName("gameList");
     const userLibrary = document.getElementById("localUserLibrary");
+    const addLibButton = document.getElementsByClassName("addToLibrary");
 })
 
 if (localStorage.getItem("userLibrary") == null) {
