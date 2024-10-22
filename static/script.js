340ea980c729c65a1a3c8072ab444febccb3c4a2
Amr-Abdelbar <amrfabdelbar@gmail.com>
2024-10-22 07:04:35 +0300

    small stuff

--- static/script.js
+++ static/script.js
@@ -11,26 +11,18 @@ document.addEventListener("DOMContentLoaded", ()=> {
     const addLibButton = document.getElementsByClassName("addToLibrary");
 
     const form = document.getElementsByClassName('login')[0];
-    // console.log(form);
 
     if (!userLogged()) {
         form.style.display = 'block';
     } else {
         form.style.display =  'none';
+        welcome = document.createElement("p");
+        welcome.innerHTML = "Hey " + localStorage.getItem('userName') + "!";
     }
 
-    // const userLibrary = document.getElementById("localUserLibrary");
-
-    // if (localStorage.getItem("userLibrary") == null) {
-    //     localStorage.setItem("userLibrary", toString(userLibrary));
-    // }
-    
-    // else {
-    //     userLibrary = localStorage.getItem("userLibrary");
-    // }
 })
 
-document.getElementById('form').addEventListener('submit', (e) => {
+document.getElementById('loginInput').addEventListener('submit', (e) => {
     e.preventDefault();
 
     const loginButton = document.getElementById("loginButton");
