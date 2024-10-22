26eda7665e52a778fcbf395ac6cef22a3aebb45b
Amr-Abdelbar <amrfabdelbar@gmail.com>
2024-10-22 17:38:48 +0300

    userLibrary stuff

--- remocritic.py
+++ remocritic.py
@@ -67,7 +67,7 @@ def homepage():
 def userLibrary():
     if request.method == 'POST':
         data = request.get_json()
-        local_lib = open("localLib.JSON","r+")
+        local_lib = open("localLib.JSON","a+")
         lib_content = local_lib.read()
         
         for game in data:
@@ -82,6 +82,10 @@ def userLibrary():
             "status": 200
         })
     else:
+
+        local_lib = open("localLib.JSON","a+")
+        lib_content = local_lib.read()
+        local_lib.close()
 
         return render_template("userLibrary.html")
 
